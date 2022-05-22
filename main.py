45ca877286acbe01fcd272b09bad49aff4389869
Reda E <reda.elmasdouri@gmail.com>
2022-05-22 22:53:34 +0200

    Compteur et ToggleButton

--- main.py
+++ main.py
@@ -9,9 +9,26 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
 from numpy import size
+from kivy.properties import StringProperty
 
 class MainWidget(Widget):
     pass
+
+class WidgetExemple(GridLayout):
+    mon_texte = StringProperty("0")
+    compteur = 1
+    def on_button_click(self):
+        print("Button click")
+        self.mon_texte = str(self.compteur)
+        self.compteur += 1
+    def on_toggle_button_state(self, widget):
+        #print("Toggle state: " + widget.state)
+        if widget.state == "normal":
+            print("OFF")
+            widget.text = "OFF"
+        else:
+            print("ON")
+            widget.text = "ON"
 
 class GridLayoutExemple(GridLayout):
     pass
